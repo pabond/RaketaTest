abb75bb1bd3e3fd39ddde85ed5dc9f69b222a8f2
Pavel Bondar <bond.p85@gmail.com>
2020-08-16 18:09:20 +0300

    create project

---
+++ RaketaTest_Bondar/RaketaTest_Bondar/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RaketaTest_Bondar
+//
+//  Created by Pavel Bondar on 16.08.2020.
+//  Copyright © 2020 Coopers. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
